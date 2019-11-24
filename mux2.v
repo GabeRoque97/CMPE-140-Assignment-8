4fd68ef1c46bdde6cbd40b8c246552bccaec360a
Derek Nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-11-24 00:52:55 -0800

    Update mux2.v

--- mux2.v
+++ mux2.v
@@ -2,9 +2,32 @@ module mux2 #(parameter WIDTH = 8) (
         input  wire             sel,
         input  wire [WIDTH-1:0] a,
         input  wire [WIDTH-1:0] b, 
+        output reg [WIDTH-1:0] y
+    );
+    
+    initial
+    begin
+        y = a;
+    end
+    always@(*)
+    begin
+        case(sel)
+            1'b0:       y = a;
+            1'b1:       y = b;
+            default:    y = a;
+        endcase
+    end
+endmodule
+
+/*
+module mux2 #(parameter WIDTH = 8) (
+        input  wire             sel,
+        input  wire [WIDTH-1:0] a,
+        input  wire [WIDTH-1:0] b, 
         output wire [WIDTH-1:0] y
     );
 
     assign y = (sel) ? b : a;
 
 endmodule
+*/
