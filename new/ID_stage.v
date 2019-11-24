ff3b325ffee735916299c776f029f38e2eb9ebf2
Derek Nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-11-24 00:50:40 -0800

    Update ID_stage.v

--- new/ID_stage.v
+++ new/ID_stage.v
@@ -1,30 +1,28 @@
-`timescale 1ns / 1ps
-
 module ID_stage(
-    input wire clk,
-    input wire multu_enD,
-    input wire jr_selD,
-    input wire shiftD,
-    input wire [1:0] super_selD,
-    input wire [2:0] alu_ctrlD,
-    input wire alu_srcD,
-    input wire branchD,
-    input wire dm2regD,
-    input wire jumpD,
-    input wire [1:0] reg_dstD,
-    input wire we_dmD,
-    input wire jal_selD,
-    input wire we_regD,
-    input wire [31:0] pc_plus_4D,
-    input wire [31:0] alu_paD,
-    input wire [31:0] wd_dmD,
-    input wire [31:0] rd3D,
-    input wire [31:0] instrD,
-    input wire [31:0] sext_immD,
-    input wire [31:0] HI_qD,
-    input wire [31:0] LO_qD,
-    
-      
+        input wire clk,
+        input wire multu_enD,
+        input wire jr_selD,
+        input wire shiftD,
+        input wire [1:0] super_selD,
+        input wire [2:0] alu_ctrlD,
+        input wire alu_srcD,
+        input wire branchD,
+        input wire dm2regD,
+        input wire jumpD,
+        input wire [1:0] reg_dstD,
+        input wire we_dmD,
+        input wire jal_selD,
+        input wire we_regD,
+        input wire [31:0] pc_plus_4D,
+        input wire [31:0] alu_paD,
+        input wire [31:0] wd_dmD,
+        input wire [31:0] rd3D,
+        input wire [31:0] instrD,
+        input wire [31:0] sext_immD,
+        input wire [31:0] HI_qD,
+        input wire [31:0] LO_qD,
+        
+          
         output reg multu_enE,
         output reg jr_selE,
         output reg shiftE,
@@ -49,7 +47,8 @@ module ID_stage(
            
     );
     
-    always@(posedge clk)begin
+    always@(posedge clk)
+    begin
         //Control signals
         multu_enE <= multu_enD;
         jr_selE <= jr_selD;
@@ -62,6 +61,7 @@ module ID_stage(
         jumpE <= jumpD;
         reg_dstE <= reg_dstD;
         we_dmE <= we_dmD;
+        jal_selE <= jal_selD;
         we_regE <= we_regD;
         
         //DP signals
