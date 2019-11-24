5118ed42fd883616c86c98d6a956c277b4735484
GabeRoque97 <48231557+GabeRoque97@users.noreply.github.com>
2019-11-23 22:18:01 -0800

    Update DQReg.v

--- DQReg.v
+++ DQReg.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/23/2019 01:23:58 PM
-// Design Name: 
-// Module Name: DQReg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module DQReg(
     input wire          En,
