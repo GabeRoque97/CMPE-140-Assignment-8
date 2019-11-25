32f327a1228c6acce7ccabe39ba4b4f4464e0ca2
Derek Nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-11-25 01:07:27 -0800

    Update DQReg.v

--- DQReg.v
+++ DQReg.v
@@ -1,9 +1,9 @@
 module DQReg(
-    input wire          En,
-    input wire [31:0]   D,
-    input wire          clk,
-    output reg [31:0]   Q,
-    input wire rst
+        input wire          En,
+        input wire [31:0]   D,
+        input wire          clk,
+        output reg [31:0]   Q,
+        input wire rst
     );
     
 always @ (posedge clk, posedge rst)
