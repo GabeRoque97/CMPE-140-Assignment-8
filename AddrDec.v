c965b7467dda9468e6fca7da902d416969b61947
Derek Nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-11-25 00:57:45 -0800

    Consider module in commented portion

--- AddrDec.v
+++ AddrDec.v
@@ -27,3 +27,27 @@ module AddrDec(
         
     end     
 endmodule
+/*
+module AddrDec(
+        input wire [1:0]    A,
+        input wire          WE,
+        output wire         WE2,
+        output wire         WE1,
+        output wire [1:0]   Read_Sel
+    );
+    
+reg [1:0] ctrl;
+
+assign {WE2, WE1, Read_Sel} = ctrl;
+
+always @ (*) begin
+    case (A)
+        2'b00: ctrl = 4'b0000; //propegate gpl1 to RD
+        2'b01: ctrl = 4'b0001; //propegate gpl2 to RD
+        2'b10: ctrl = 4'b0110; //propegate gpO1 to RD
+        2'b11: ctrl = 4'b1011; //propegate gpO2 to RD
+        default: ctrl = 4'b0000;
+    endcase           
+end     
+endmodule
+*/
