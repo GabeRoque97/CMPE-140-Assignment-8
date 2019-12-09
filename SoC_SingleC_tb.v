3e319516d3a72cab249c761931fa7ef2b32cb6f5
GabeRoque97 <48231557+GabeRoque97@users.noreply.github.com>
2019-12-08 22:04:49 -0800

    Update SoC_SingleC_tb.v

--- SoC_SingleC_tb.v
+++ SoC_SingleC_tb.v
@@ -2,50 +2,63 @@
 
 module SoC_SingleC_tb;
     reg clk, rst;
-    reg [31:0] gpIn1;
+    reg [3:0] switches;
+    reg       Sel;
+    reg [31:0] ra3;
     wire [31:0] gpOut1, gpOut2;
-    reg [31:0] ra3; 
-    wire [31:0] rd3; 
+    wire [31:0] rd3;
     wire dispSe, factErr;
+    wire [31:0]   gpIn1;
     
-    system DUT( 
-        .clk (clk),
-        .rst (rst),
-        .ra3(ra3),
-        .rd3(rd3),
-        .gpI1 (gpIn1),
-        .gpO1 (gpOut1),
-        .gpO2 (gpOut2),
-        .gpI2 (gpOut1)
-    );
+    assign gpIn1 = {27'b0,Sel,switches[3:0]};
     
     assign dispSe = gpOut1[4];
     assign factErr = gpOut1[0];
     
-    integer i,j;
+    system DUT( 
+        .clk (clk),
+        .rst (rst),
+        .gpI1 (gpIn1),
+        .gpO1 (gpOut1),
+        .gpO2 (gpOut2),
+        .gpI2 (gpOut1),
+        .ra3    (ra3),
+        .rd3    (rd3)
+    );
+    
+   //if(DUT.fact_top.FA.done == 1)
+   
+    
+    integer i,j,k;
     
     initial begin
         rst = 1;
         tick;
         rst = 0;
-        ra3 <= 'h09; 
-        for(i = 0; i < 31; i = i + 1)begin
-            gpIn1 = i; // Sel = 0 & n = 0 to 15
+        tick;
+        tick;
+       
+        for(j = 0; j < 2; j = j + 1)begin   
+            Sel = j;    
+            
+            for(i = 0; i < 16; i = i + 1)begin
+                
+                switches = i;
          
-            for(j = 0; j < 19; j = j + 1)begin 
-                tick;//clock through the driver code
-            end
+                 for(k = 0; k < 40; k = k + 1)begin 
+                    tick;//clock through the driver code
+                end
         
+            end
         end
-    
     //while(factErr == 0) tick;
     
     end
     
     task tick; 
     begin  
-            clk <= 0; #5;
-            clk <= 1; #5; 
+            clk <= 0; #10;
+            clk <= 1; #10; 
             end
     endtask
 endmodule
